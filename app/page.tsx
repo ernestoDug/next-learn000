7cb892a727cf6770980787e1c2ebb515f50d0179
Ev Bludov <120216330+ernestoDug@users.noreply.github.com>
2024-10-06 01:22:35 +0300

    3

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,23 @@
-import AcmeLogo from '../app/ui/acme-logo';
+import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+// імпортую стилі для модулів 
+import styles from '@/app/ui/home.module.css';
+
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+        <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
         
-        <div
-  className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
-/>
+        <div className={styles.shape} />
+        {/* назва імпорта а шейп то клас  */}
+        <p className={styles.myCls}>мій клас модуль стиль</p>
+
 
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Привіт.</strong>  Тут ти будеш вчитись {' '}
@@ -25,7 +29,7 @@ export default function Page() {
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Зарегся</span> <ArrowRightIcon className="w-3 md:w-2" />
+            <span>Зарегся</span> <ArrowRightIcon className="w-30 md:w-2" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -6,8 +6,8 @@ export default function AcmeLogo() {
     <div
       className={`flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-1 w-1 rotate-[90deg]" />
-      <p className="text-[44px]">**********</p>
+      <GlobeAltIcon className="h-10 w-10 rotate-[90deg]" />
+      <p className="text-[44px]"></p>
     </div>
   );
 }
