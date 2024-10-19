ddde10a03d4802a542d08a89ee16ae9ab445ae25
Ev Bludov <120216330+ernestoDug@users.noreply.github.com>
2024-10-19 13:11:06 +0300

    add usepathname and Links

--- README.md
+++ README.md
@@ -10,6 +10,19 @@ For more information, see the [course curriculum](https://nextjs.org/learn) on t
  <!-- Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted -->
 
  cd nextjs-dashboard
-  pnpm run dev
 
-  https://nextjs.org/learn/dashboard-app/optimizing-fonts-images#why-optimize-images 06/10
+ <!-- все що з навігацією повязано в ю ай теці в нав лінкс  -->
+  pnpm run dev
+  
+<!-- посібник гіт хаб -->
+  <!-- https://help.github.com/en/github/getting-started-with-github/create-a-repo -->
+  
+  
+  
+  
+  https://nextjs.org/learn/dashboard-app/optimizing-fonts-images#why-optimize-images 
+  12/10
+  5: Навігація між сторінками
+
+
+
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,35 +1,63 @@
+// це тому що ми тут використувуюме ум усю патх нейм хуки завжди клієнтьски
+'use client';
+import { usePathname } from 'next/navigation';
+
+import clsx from 'clsx';
+// це бібліотека сстилів для умовних класів активне чи ні 
+// а хук юс патх нейм допогмає вказувати де були де є  
+
 import {
   UserGroupIcon,
-  HomeIcon,
+  HomeModernIcon,
   DocumentDuplicateIcon,
+  BuildingStorefrontIcon,
 } from '@heroicons/react/24/outline';
+// дамєо назви іконкам без дефісів і слово айкон
+// https://heroicons.com/outline
+
+import Link from 'next/link';
+// це щоб навігація була без зайвих оновлень 
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  { name: 'Додому', href: '/dashboard', icon: HomeModernIcon },
   {
-    name: 'Invoices',
+    name: 'Рахунки',
     href: '/dashboard/invoices',
     icon: DocumentDuplicateIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Клієнти', href: '/dashboard/customers', icon: UserGroupIcon },
+
+  // ++++++++++++++++++++++мой
+  { name: 'Мой бредик', href: '/dashboard/mytec', icon: BuildingStorefrontIcon },
 ];
 
+
+
+
 export default function NavLinks() {
+  const pathname = usePathname();
+
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          // цей лінк замінив тег а
+          <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
-          >
+            // ось класи щоб активні посилані робити іншим кольором 
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href,
+              },
+            )}          >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
